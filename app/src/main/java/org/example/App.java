0560034edf12ab3479fccafaaad57cb605ea7a34
fortisvariano <vf41922@mail.harpercollege.edu>
2025-01-26 23:17:35 -0600

    Code for App.java and Converter.java

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,42 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
+    System.out.println("Welcome to my ounces to pounds converter");
+
+    boolean repeat = true;
+
+    int ouncesInput = Integer.MIN_VALUE;
+    Scanner scanner = new Scanner(System.in);
+
+    do {
+      System.out.print("Enter number of ounces to convert to pounds: ");
+      String input = scanner.nextLine().trim();
+      if(input.isBlank()) {
+        System.out.println("Blank values are not valid");
+      } else try {
+        ouncesInput = Integer.parseInt(input);
+        if (ouncesInput < 0) {
+          System.out.println("Negative numbers are not valid");
+          ouncesInput = Integer.MIN_VALUE;
+        } else {
+          repeat = false;
+        }
+      } catch(NumberFormatException e) {
+        System.out.println("Non-integers and letters are not valid");
+        ouncesInput = Integer.MIN_VALUE;
+      }
+      System.out.println();
+    } while(repeat);
+
+    scanner.close();
+
+    Converter converter = new Converter();
+    System.out.println(ouncesInput + " oz is equivalent to " + converter.toPounds(ouncesInput));
+    System.out.println(ouncesInput + " oz is equivalent to " + converter.toPoundsAndOunces(ouncesInput));
+    System.out.println();
+    System.out.println("Exiting...");
   }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,5 +1,25 @@
 package org.example;
 
+import java.text.DecimalFormat;
+
 public class Converter {
-  // Your code goes here
+    public String toPounds(int ounces) {
+        DecimalFormat decimalFormat = new DecimalFormat("0.0000");
+        double pounds = ounces / 16d;
+        String poundsString = decimalFormat.format(pounds) + " lb";
+        if(pounds != 1) {
+            poundsString += 's';
+        }
+        return poundsString;
+    }
+    public String toPoundsAndOunces(int ounces) {
+        int pounds = ounces / 16;
+        int ouncesR = ounces % 16;
+        String poundsAndOuncesString = pounds + " lb";
+        if(pounds != 1) {
+            poundsAndOuncesString += 's';
+        }
+        poundsAndOuncesString += " " + ouncesR + " oz";
+        return poundsAndOuncesString;
+    }
 }
